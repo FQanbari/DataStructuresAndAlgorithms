b19f6bd673f966fee5429425e689bb16f4c967a9
FQanbari <fatemeh.ganbari1993@gmail.com>
2022-02-01 20:30:13 +0330

    Linkedlist exercise addFirst addLast deleteFirst deleteLast contains indexOf size toArray reverse getKthFromTheEnd printMiddle hasLoop

--- Program.cs
+++ Program.cs
@@ -1,57 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DataStructures
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            var array = new Array(3);
-            array.Insert(2);
-            array.Insert(7);
-            array.Insert(33);
-            array.Insert(5);
+            var linkedList = new LinkedList();
+            //Console.WriteLine("size : {0}", linkedList.Size());
+            linkedList.AddFirst(7);
+            linkedList.AddFirst(33);
+            linkedList.AddLast(13);
+            linkedList.AddLast(3);
+            linkedList.AddLast(4);
+            linkedList.AddLast(27);
+            linkedList.AddLast(29);
 
-            var array2 = new Array(3);
-            array2.Insert(2);
-            array2.Insert(7);
-            array2.Insert(0);
-            array2.Insert(43);
-            array2.Insert(11);
-            array2.Insert(41);
+            //Console.WriteLine("size : {0}", linkedList.Size());
+            //linkedList.DeleteFirst();
+            //Console.WriteLine(linkedList.IndexOf(93));
+            //Console.WriteLine(linkedList.Contains(7));
+            //linkedList.DeleteLast();
+            //Console.WriteLine("size : {0}", linkedList.Size());
+            //var str = linkedList.ToArray();
+            //foreach (var item in str)
+            //    Console.WriteLine(item);
 
-            Console.WriteLine("Print Array:");
-            array.Print();
-            Console.WriteLine("----------------");
-
-            array.InsertAt(23, 5);
-            array.InsertAt(14, 7);
-            Console.WriteLine("Insert At: 23 in 5th item and 14 in 7th item");            
-            array.Print();
-            Console.WriteLine("----------------");
-
-            var reverse = array.Reverse();
-            Console.WriteLine("Reverse Array: ");
-            reverse.Print();
-            Console.WriteLine("----------------");
-
-            
-            Console.WriteLine("Maximum Array: {0}", array.Max());
-            array.Print();
-            Console.WriteLine("----------------");
-
-            Console.WriteLine("Index Of: {0}", array.IndexOf(5));
-            Console.WriteLine("----------------");
-
-            array.RemoveAt(5);
-            Console.WriteLine("Remove 5th item");
-            array.Print();
-            Console.WriteLine("----------------");
-
-            var result = array.Intersect(array2);
-            Console.WriteLine("Intrersect array: ");
-            result.Print();
-            Console.WriteLine("----------------");
+            //linkedList.Reverse();
+            //var str0 = linkedList.ToArray();
+            //foreach (var item in str0)
+            //    Console.WriteLine(item);
+            //var kth = linkedList.GetKthFromTheEnd(3);
+            //Console.WriteLine("3th from the end is {0}", kth);
+            //linkedList.PrintMiddle();
+            //var result = (linkedList.HasLoop());
 
             Console.ReadLine();
         }
