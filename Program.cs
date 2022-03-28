9424fe7fae8876435ce33a1b3f5f754122cefeb7
FQanbari <fatemeh.ganbari1993@gmail.com>
2022-03-28 21:46:49 +0430

    Implement a Heap with Max, insert, remove, heapify MaxHeap Implement with inHeapify minHeap implement with insert, remove, isEmpty

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
 namespace DataStructures
 {
@@ -6,16 +7,103 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            var input = new int[] { 10, 5, 15, 6, 1, 8, 12, 18, 17 };
-            var input2 = new int[] { 20, 10, 6, 14, 3, 8, 30, 24, 26 };
-            var input3 = new int[] { 7, 4, 9, 1, 6, 8, 10 };
-            var input4 = new int[] { 10, 30, 20 };
-            var tree = new AVLTree();
-            foreach (var item in input3)
-                tree.insert(item);
-            var isPerfect = tree.isPerfect();
-            var isBalance = tree.isBalance();
+            int[] numbers = { 5, 3, 8, 4, 1, 2 };
+            string[] charnumber = { "5", "3", "8", "4", "1", "2" };
+            var minHeap = new minHeap(10);
+            foreach(var item in charnumber)
+                minHeap.Add(item, Convert.ToInt32(item));
+
+            Console.WriteLine(minHeap.Remove());
+            Console.WriteLine(minHeap.Remove());
+            Console.WriteLine(minHeap.Remove());
+            Console.WriteLine(minHeap.Remove());
         }
     }
+    public class minHeap
+    {
+        private class node
+        {
+            public string value;
+            public int key;
+        }
+        private node[] items;
+        private int size;
+        public minHeap(int size)
+        {
+            items = new node[size];
+        }
+        public void Add(string item, int key)
+        {
+            if (size == items.Length)
+                throw new IndexOutOfRangeException();
+
+            items[size] = new node
+            {
+                value = item,
+                key = key
+            };
+            size++;
+
+            ReCalculateUp();
+        }
+        private int GetLeftChildIndex(int elementIndex) => 2 * elementIndex + 1;
+        private int GetRightChildIndex(int elementIndex) => 2 * elementIndex + 2;
+        private int GetParentIndex(int elementIndex) => (elementIndex - 1) / 2;
+
+        private bool HasLeftChild(int elementIndex) => GetLeftChildIndex(elementIndex) < size;
+        private bool HasRightChild(int elementIndex) => GetRightChildIndex(elementIndex) < size;
+        private bool IsRoot(int elementIndex) => elementIndex == 0;
+
+        private void ReCalculateUp()
+        {
+            var index = size - 1;
+            while (!IsRoot(index) && items[index].key < items[GetParentIndex(index)].key)
+            {
+                var parentIndex = GetParentIndex(index);
+                Swap(parentIndex, index);
+                index = parentIndex;
+            }
+        }
+        private void ReCalculateDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                var smallerIndex = GetLeftChildIndex(index);
+                if (HasRightChild(index) && GetRightChildIndex(index) < GetLeftChildIndex(index))
+                {
+                    smallerIndex = GetRightChildIndex(index);
+                }
+
+                if (items[smallerIndex].key >= items[index].key)
+                {
+                    break;
+                }
+
+                Swap(smallerIndex, index);
+                index = smallerIndex;
+            }
+        }
+        private void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+        }
+        public string Remove()
+        {
+            if (size == 0)
+                throw new IndexOutOfRangeException();
+
+            var result = items[0];
+            items[0] = items[size - 1];
+            size--;
+
+            ReCalculateDown();
+
+            return result.value;
+        }
+    }
+
 }
 
