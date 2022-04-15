60900d490a0eb6c43eae25dfb72e2655a0ec68d4
FQanbari <fatemeh.ganbari1993@gmail.com>
2022-04-15 20:22:31 +0430

    Implement Graph this Graph has contain these functions:     - addNode     - addEdge     - print     - removeNode     - removeEdge     - traverseDepthFirst : recursion / traverse     - traverseBearthFirst     - topologicalSort     - hasCycle

--- Program.cs
+++ Program.cs
@@ -7,19 +7,46 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            string[] words = { "boy", "book", "border", "cat", "dog", "doctor", "fine", "finest", "figure", "pick", "pickle", "picture" };
-            string[] words3 = { "cat", "doctor", "fine", "finest", "figure", "pick", "pickle", "picture" };
-            string[] words2 = { "boy", "book" };
-            string[] words4 = { "fine", "finest", "figure" };
-            string[] words5 = { "pick", "pickle", "picture" };
-            var trie = new Trie();
+            var graph = new Graph();
+            //graph.addNode("A");
+            //graph.addNode("B");
+            //graph.addNode("C");
+            //graph.addNode("D");
+            //graph.addNode("E");
 
-            foreach (var w in words5)
-                trie.insert(w);
+            //graph.addEdge("A", "B");
+            //graph.addEdge("A", "E");
 
-            Console.WriteLine("prefix: " + trie.longestCommonPrefix());
-            Console.WriteLine(trie.contains("book"));
-            Console.WriteLine("count: " + trie.countWords());
+            //graph.addEdge("C", "A");
+            //graph.addEdge("C", "B");
+            //graph.addEdge("C", "D");
+
+            //graph.addEdge("B", "E");
+
+            //graph.addEdge("D", "E");
+
+            //graph.removeNode("D");
+            //graph.removeEdge("C", "B");
+            //graph.addNode("X");
+            //graph.addNode("B");
+            //graph.addNode("A");
+            //graph.addNode("P");
+
+            //graph.addEdge("X", "A");
+            //graph.addEdge("X", "B");
+            //graph.addEdge("A", "P");
+            //graph.addEdge("B", "P");
+            graph.addNode("A");
+            graph.addNode("B");
+            graph.addNode("C");
+
+            graph.addEdge("A", "B");
+            graph.addEdge("B", "C");
+            graph.addEdge("C", "A");
+            graph.print();
+
+            Console.WriteLine("**************");
+            Console.WriteLine("has Cycle: "+graph.hasCycle());
         }
     }
 }
