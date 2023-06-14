b5cc8c0d4d2b4c5bbf8d893a192f40a32e321a58
FQanbari <fatemeh.ganbari1993@gmail.com>
2023-06-14 09:29:37 +0330

    BinarySearch

---
+++ DataStracturesAndAlgorithms/Algorithms/BinarySearch.cs
@@ -0,0 +1,23 @@
+﻿namespace Algorithms;
+
+public class BinarySearch
+{
+    public static int Play(int[] array, int goal)
+    {
+        if (array == null || array.Length == 0) return -1;
+
+        var low = 0;
+        var high = array.Length - 1;
+        
+        while (low <= high)
+        {
+            var mid = (low + high) / 2;
+            var guess = array[mid];
+            if(guess == goal) return mid;
+            if(guess > goal) high = mid - 1;
+            else low = mid + 1;
+
+        }
+        return -1;
+    }
+}
---
+++ DataStracturesAndAlgorithms/Algorithms/Program.cs
@@ -0,0 +1,5 @@
+﻿using Algorithms;
+
+var array = new int[] { 1, 2, 3, 5,  7, 9  };
+
+Console.WriteLine(BinarySearch.Play(array, 6));
