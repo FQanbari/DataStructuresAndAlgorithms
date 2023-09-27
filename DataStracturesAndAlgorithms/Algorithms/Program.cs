535f3a78550dc1de272c8f34fbbbc57cb03deb22
FQanbari <fatemeh.ganbari1993@gmail.com>
2023-09-27 19:41:13 +0330

    Quick Sort Algorithm

--- DataStracturesAndAlgorithms/Algorithms/Program.cs
+++ DataStracturesAndAlgorithms/Algorithms/Program.cs
@@ -1,13 +1,8 @@
-﻿using Algorithms;
+﻿
 
+using Algorithms;
 
-var array = new int[] { 6, 8,2, 1,3,5,7,4 };
-array = SelectionSort.Sort(array);
-for (int i = 0; i < array.Length; i++)
-    Console.WriteLine(array[i]);
+var array = new int[] { 2, 6, 9, 3, 5, 4, 1 };
 
-Console.WriteLine($"index: {BinarySearch.Search(array, 6)}");
+Console.WriteLine(QuickSort.Count(array));
 
-Recursive.CountDown(3);
-
-Console.WriteLine(Recursive.Fact(5));
---
+++ DataStracturesAndAlgorithms/Algorithms/QuickSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms;
+
+public static class QuickSort
+{
+    public static int Sum(int[] arr)
+    {
+        if(arr.Length == 0) return 0;
+
+        return arr[0] + Sum(arr[1..]);
+    }
+
+    public static int Count(int[] arr)
+    {
+        if (arr.Length == 0) return 0;
+
+        return 1 + Count(arr[1..]);
+    }
+}
