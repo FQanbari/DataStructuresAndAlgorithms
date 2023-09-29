1516e9618916345d62934d600ef460b853156ed7
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-09-29 10:17:01 +0330

    Hash Table Algorithms

---
+++ DataStracturesAndAlgorithms/Algorithms/HashTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms;
+
+public class HashTable
+{
+    public static Dictionary<string, bool> _voted = new Dictionary<string, bool>();
+    public static void CheckVote(string name)
+    {
+        if (_voted.ContainsKey(name)) 
+        {
+            Console.WriteLine($"Kick {name} out");
+            return;
+        };
+
+        _voted.Add(name, true);
+        Console.WriteLine($"let {name} vote!");
+    }
+}
--- DataStracturesAndAlgorithms/Algorithms/Program.cs
+++ DataStracturesAndAlgorithms/Algorithms/Program.cs
@@ -3,8 +3,9 @@
 using Algorithms;
 
 var array = new int[] { 2, 6, 9, 3, 5, 4, 1 };
-QuickSort.Sort(array,0,6);
+HashTable.CheckVote("Fatemeh");
+HashTable.CheckVote("Fatemeh");
+HashTable.CheckVote("John");
+HashTable.CheckVote("Salma");
 
-foreach (var item in array)
-    Console.WriteLine(item);
 
